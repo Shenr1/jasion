dc8e0dfe3babc8c41840dc3be0303395aabbfa9e
Ryen <1278323157@qq.com>
2023-05-18 09:31:58 +0800

    同步线上

---
+++ README.md
@@ -0,0 +1,10 @@
+# Heybike
+
+## 主题配置拉取
+`shopify theme pull`
+
+## Shopify服务启动
+`shopify theme serve`
+
+## Tailwind监听
+`npx tailwindcss -i ./assets/app-tailwind.css -o ./assets/custom.css --watch`
---
+++ tailwind.config.js
@@ -0,0 +1,32 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './layout/*.liquid',
+    './templates/*.liquid',
+    './templates/customers/*.liquid',
+    './sections/*.liquid',
+    './snippets/*.liquid',
+  ],
+  corePlugins: {
+    preflight: false,
+    tableLayout: false
+  },
+  purge: false,
+  theme: {
+    extend: {
+      colors: {
+        vi: {
+          light: '#fcb10f',
+          DEFAULT: '#fcb10f',
+          dark: '#de9a07',
+        },
+        viGray: {
+          light: '#dbdbda',
+          DEFAULT: '#dbdbda',
+          dark: '#dbdbda',
+        }
+      }
+    },
+  },
+  plugins: []
+}
