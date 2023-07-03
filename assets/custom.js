135a266ab1225fa90a441df57f135b3ef0543ae9
Ryen <1278323157@qq.com>
2023-07-03 18:20:40 +0800

    完成至参数

--- assets/custom.js
+++ assets/custom.js
@@ -1,6 +1,9 @@
 $(function(){
-
-
-
-
+$('.switch_tabs .item-tab').click(function() {
+    var tabNumber = $(this).data('tab');
+    $('.switch_tabs .item-content').addClass('tw-hidden');
+    $('.switch_tabs .item-content[data-tab="' + tabNumber + '"]').removeClass('tw-hidden');
+    $('.switch_tabs .item-tab').removeClass('active');
+    $(this).addClass('active');
+    });
 })
