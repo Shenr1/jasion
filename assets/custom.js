d2b687d39b458b9da05786f4bd04ca1e1eea1b7f
Ryen <1278323157@qq.com>
2023-07-17 11:29:44 +0800

    1024-750处理

--- assets/custom.js
+++ assets/custom.js
@@ -1,9 +1,20 @@
+// 适配
+const autoReponsive = () => {
+    const width = document.body.clientWidth || window.innerWidth;
+    if (width < 1025 && width >= 750) {
+        const scale = width / 1023;
+        const content = 'width=1023, initial-scale=' + scale + ', minimum-scale=' + scale + ', maximum-scale=' + scale + ', viewport-fit=cover';
+        $('meta[name="viewport"]').attr("content",content);
+    }
+}
+autoReponsive()
+
 $(function(){
-$('.switch_tabs .item-tab').click(function() {
-    var tabNumber = $(this).data('tab');
-    $('.switch_tabs .item-content').addClass('tw-hidden');
-    $('.switch_tabs .item-content[data-tab="' + tabNumber + '"]').removeClass('tw-hidden');
-    $('.switch_tabs .item-tab').removeClass('active');
-    $(this).addClass('active');
+    $('.switch_tabs .item-tab').click(function() {
+        var tabNumber = $(this).data('tab');
+        $('.switch_tabs .item-content').addClass('tw-hidden');
+        $('.switch_tabs .item-content[data-tab="' + tabNumber + '"]').removeClass('tw-hidden');
+        $('.switch_tabs .item-tab').removeClass('active');
+        $(this).addClass('active');
     });
 })
